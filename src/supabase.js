cba5395ff8c3ce9ed87aff1e1d52a796c4fa348a
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-07-02 19:46:04 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,13 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 import { createClient } from '@supabase/supabase-js';
 
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL or Key is missing');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
