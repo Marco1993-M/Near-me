7aa602c0581d66194e3ef6842cad58324d9719c2
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-07-02 20:05:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import { initSearch } from './search.js';
 
 const supabaseUrl = 'https://mqfknhzpjzfhuxusnasl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZmtuaHpwanpZmY0V3dXNuYXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4MjU5NTYsImV4cCI6MjA2MzQwMTk1Nn0.m2g3hmoHttl9baVt3VW4tTMt3jQc_toN5iwuYbZfisg2m';
+
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseKey);
+
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 // Trigger search initSearch after Google Maps API loads
