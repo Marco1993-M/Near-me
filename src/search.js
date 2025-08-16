05190939eecdda72db9af668265e25dcef3eb9ea
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-08-16 10:17:52 +0200

    Update search.js

--- src/search.js
+++ src/search.js
@@ -58,6 +58,7 @@ export function initSearch(supabase) {
         renderSearchResults({ cities, places }, searchDropdown);
       } catch (error) {
         console.error('Error searching places:', error);
+        console.error('MIME Error:', error);
         searchDropdown.classList.add('hidden');
       }
     }, 300)
