1f7ae3456deccf328ed225c4f415a27ce36f1c85
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-08-16 10:31:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    historyApiFallback: true,
-  },
-  // Add any other necessary configurations here
+  // clean slate, no PWA / no fallback
 });
