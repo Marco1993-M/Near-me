9211e79fb1461ae0ba007dfb8b9e4a6cf03ad81d
Marco1993-M <136054712+Marco1993-M@users.noreply.github.com>
2025-09-05 12:58:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { initReviews } from './reviews.js';
 import { initSearch } from './search.js';
 import { initTasteProfile } from './quiz.js';
 import { checkNearbyShops } from './shopPopUp.js';
-import { showToast } from './UI.js';
+import { showToast } from './ui.js';
 
 const supabaseUrl = 'https://mqfknhzpjzfhuxusnasl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZmtuaHpwanpZmY0V3dXNuYXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4MjU5NTYsImV4cCI6MjA2MzQwMTk1Nn0.m2g3hmoHttl9baVt3VW4tTMt3jQc_toN5iwuYbZfisg2m';
